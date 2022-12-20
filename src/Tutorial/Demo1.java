abc0bbaed3939661620321f9dda1a1146fd7044a
unknown <kokareshital34@gmail.com>
2022-12-20 17:31:04 +0530

    message first

---
+++ src/Tutorial/Demo1.java
@@ -0,0 +1,9 @@
+package Tutorial;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("helooooooooooooooo");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Shital Kokare
+ *
+ */
+module Technicalproject {
+}
